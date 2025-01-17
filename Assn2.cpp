67cffce79a29bcee3572cf91c8ab02c4b9e2aa55
JunKang <xenoficx69420@outlook.sg>
2025-01-17 13:58:09 +0800

    yes

--- Assn2.cpp
+++ Assn2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main() {
     vector<ShapeTwoD*> shapes;
     int choice;
+    string errorMessage;
 
     do {
         cout << "\n---------------------------------------------" << endl;
@@ -24,19 +25,62 @@ int main() {
         cout << endl;
         cout << "Please enter your choice: ";
         cin >> choice;
+        cout << endl;
+        if(!errorMessage.empty()) {
+            cout << errorMessage;
+        }
 
         if(choice == 1) {
-            string name;
+            string shapeName;
             string warpSpace;
-            int centerX, centerY, width, height;
+            bool canWarpSpace;
+            int verticeCount;
+            string x, y;
 
             cout << "[ Input sensor data ]" << endl;
             cout << "Please enter name of shape : ";
-            cin >> name;
+            cin >> shapeName;
 
             cout << "Please enter special type : ";
             cin >> warpSpace;
-            bool containsWarpSpace = (warpSpace == "WS");
+
+            if (warpSpace == "WS") {
+                canWarpSpace = true;
+            } else if (warpSpace == "NS") {
+                canWarpSpace == false;
+            } else {
+                errorMessage = "Invalid type, Please use NS or WS.";
+            }
+
+            if (shapeName == "Square" || shapeName == "Rectangle") {
+                verticeCount = 4;
+                for (int i=0; i<verticeCount; i++){
+                    cout << "Please enter x-ordinate of pt. " << i+1 << ": ";
+                    cin >> x;
+                    cout << "Please enter y-ordinate of pt. " << i+1 << ": ";
+                    cin >> y;
+                    cout << endl;
+
+                    //add checker to check if input is a number
+                    if (!isdigit(stoi(x))){
+                        errorMessage = "Invalid input for x : " + x;
+                        if (stoi(x) < 0) {
+                            errorMessage = "Negative Coords for x : " + x;
+                        }
+                    } else if (!isdigit(stoi(y))) {
+                        errorMessage = "Invalid input for y : " + y;
+                        if (stoi(y) < 0) {
+                            errorMessage = "Negative Coords for y: " + y;
+                        }
+                    }
+
+
+                    //How to store the coordinate properly?
+
+
+                }
+            }
+            
 
             
 
