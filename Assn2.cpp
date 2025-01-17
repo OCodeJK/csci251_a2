cd1bc579ba3bc20c4ab79b7b53adb5ed1cf066df
JunKang <xenoficx69420@outlook.sg>
2025-01-17 12:49:08 +0800

    br

--- Assn2.cpp
+++ Assn2.cpp
@@ -1,8 +1,59 @@
 #include <string>
+#include <vector>
+#include <iostream>
+#include "class.cpp"
 
-
+using namespace std;
 
 int main() {
+    vector<ShapeTwoD*> shapes;
+    int choice;
 
-    return 0;
+    do {
+        cout << "\n---------------------------------------------" << endl;
+        cout << "Student ID   : 9071180" << endl;
+        cout << "Student Name : Ooi Jun Kang" << endl;
+        cout << "---------------------------------------------" << endl;
+        cout << "Welcome to Assn2 program!" << endl;
+        cout << endl;
+        cout << "1)   Input sensor data" << endl;
+        cout << "2)   Compute area (for all records)" << endl;
+        cout << "3)   Print shapes report" << endl;
+        cout << "4)   Sort shapes data" << endl;
+        cout << "5)   Quit" << endl;
+        cout << endl;
+        cout << "Please enter your choice: ";
+        cin >> choice;
+
+        if(choice == 1) {
+            string name;
+            string warpSpace;
+            int centerX, centerY, width, height;
+
+            cout << "[ Input sensor data ]" << endl;
+            cout << "Please enter name of shape : ";
+            cin >> name;
+
+            cout << "Please enter special type : ";
+            cin >> warpSpace;
+            bool containsWarpSpace = (warpSpace == "WS");
+
+            
+
+
+        } else if (choice == 2) {
+
+        } else if (choice == 3) {
+
+        } else if (choice == 4) {
+            
+        }
+
+    } while (choice != 5);
+
+    for (auto shape : shapes){
+        delete shape;
+    }
+
+    return 0;   
 }
--- class.cpp
+++ class.cpp
@@ -30,7 +30,7 @@ class ShapeTwoD {
         }
 
         //Pure virtual methods to be overidden by derived classes
-        virtual double computeArea() const = 0;
+        virtual double computeArea() = 0;
         virtual bool isPointInShape(int x, int y) const = 0;
         virtual bool isPointOnShape(int x, int y) const = 0;
 
@@ -48,4 +48,37 @@ class ShapeTwoD {
         //Virtual Destructor
         virtual ~ShapeTwoD(){}
         
+};
+
+
+class Square : public ShapeTwoD {
+    private:
+        int x,y;
+        int length;
+    
+    public:
+        Square(string name, bool containsWarpSpace, int x, int y, int length)
+         : ShapeTwoD(name, containsWarpSpace), x(x), y(y), length(length) {}
+        
+        double computeArea() override {
+            return length * length;
+        }
+
+        bool isPointInShape(int px, int py) const override {
+        int halfLength = length / 2;
+        return (px > x - halfLength && px < x + halfLength &&
+                py > y - halfLength && py < y + halfLength);
+        }
+
+        bool isPointOnShape(int px, int py) const override {
+        int halfLength = length / 2;
+        return (px == x - halfLength || px == x + halfLength) && (py >= y - halfLength && py <= y + halfLength) ||
+               (py == y - halfLength || py == y + halfLength) && (px >= x - halfLength && px <= x + halfLength);
+        }
+
+        string toString() override {
+        return ShapeTwoD::toString() +
+               "\nCenter: (" + std::to_string(x) + ", " + std::to_string(y) +
+               ")\nLength: " + std::to_string(length);
+        }
 };
